16d14bd6038b7a3178fe3bac10c455bb719df154
Perrin Owens <po00032@my.westga.edu>
2024-11-08 17:57:13 -0500

    Did the checking account.cs

---
+++ CheckingAccount.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3300Assignment9PerrinOwens
+{
+    public class CheckingAccount : BankAccount
+    {
+        public CheckingAccount(string ownerName, decimal balance, string monthOpened, double interestRate, string type)
+            : base(ownerName, balance, monthOpened, interestRate, type)
+        {
+        }
+
+        public override decimal CalculateMinimumBalanceFee()
+        {
+            if (Balance < 1000)
+            {
+                return Balance * 0.10m;
+            }
+
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Account Type: {Type} - {base.ToString()}";
+        }
+    }
+}
