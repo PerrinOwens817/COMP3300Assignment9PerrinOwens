11ee691305773ffbb071087b4b3b8bf62f5059c1
Perrin Owens <po00032@my.westga.edu>
2024-11-08 17:49:08 -0500

    Did the savings account.cs

---
+++ SavingsAccount.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP3300Assignment9PerrinOwens
+{
+    public class SavingsAccount : BankAccount
+    {
+        public SavingsAccount(string ownerName, decimal balance, string monthOpened, double interestRate, string type)
+            : base(ownerName, balance, monthOpened, interestRate, type)
+        {
+        }
+
+        public override decimal CalculateMinimumBalanceFee()
+        {
+            if (Balance < 1500)
+            {
+                return Balance * 0.05m;
+            }
+            return 0;
+        }
+
+        public override string ToString()
+        {
+            return $"Account Type: {Type} - {base.ToString()}";
+        }
+    }
+}
